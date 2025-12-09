8498a71cad4820e82444fa8e8cef1fa5eef4cea7
Eduardo Lago <laguillo@gmail.com>
2025-12-09 16:28:03 -0500

    feat: actualizar el esquema de Prisma para incluir campos de baneo en el modelo User y mejorar la configuración del cliente de autenticación

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
-import { createAuthClient } from 'better-auth/react';
+import { createAuthClient } from 'better-auth/client';
+import { adminClient } from 'better-auth/client/plugins';
 
 export const authClient = createAuthClient({
-  //you can pass client configuration here
+  plugins: [adminClient()]
 });

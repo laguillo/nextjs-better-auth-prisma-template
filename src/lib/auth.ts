0430fabcc598b116c13c60507e52ce6ad41396e8
Eduardo Lago <laguillo@gmail.com>
2025-12-09 16:20:31 -0500

    feat: agregar soporte para cookies en la autenticación

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { nextCookies } from 'better-auth/next-js';
 import prisma from '@/lib/prisma';
 
 export const auth = betterAuth({
@@ -14,5 +15,6 @@ export const auth = betterAuth({
   },
   database: prismaAdapter(prisma, {
     provider: 'postgresql'
-  })
+  }),
+  plugins: [nextCookies()]
 });

0977065dac69126eb58b44329006d8ecbef3b645
Eduardo Lago <laguillo@gmail.com>
2025-12-08 21:57:40 -0500

    feat: agregar mejor soporte de autenticación con better-auth y Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
+import { PrismaClient } from '@/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client/extension';
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
@@ -15,6 +15,8 @@ const prisma =
     adapter
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
